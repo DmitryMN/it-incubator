3745d61bbd5eb25a1189e258e8b2d8688f931897
Malenkov Dmitrii <m18dm.dm@gmail.com>
2021-10-03 21:22:10 +0300

    App add in git

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+type AccordionProps = {
+    title: string
+}
+
+function Accordion(props: AccordionProps) {
+    return (
+        <div>
+            <AccordionTitle title={props.title}/>
+            <AccordionBody/>
+        </div>
+    );
+}
+
+function AccordionTitle(props: AccordionProps) {
+    return <h3>{props.title}</h3>
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
+export default Accordion;
---
+++ src/components/Raiting/Raiting.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+type RatingProps = {
+    value: number
+}
+
+function Rating(props: RatingProps) {
+    if(props.value === 1) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        );
+    } else if(props.value === 2) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        );
+    } else if(props.value === 3) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        );
+    } else if(props.value === 4) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+        );
+    } else if(props.value === 5) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        );
+    }
+    return (
+        <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    );
+}
+
+type StarProps = {
+    selected: boolean
+}
+
+function Star(props: StarProps) {
+    if (props.selected) {
+        return <span><b>star </b></span>
+    }
+    return <span>star </span>
+}
+
+export default Rating;
